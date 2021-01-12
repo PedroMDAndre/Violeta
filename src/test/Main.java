a3aeeab4c7c8d49a047b10f55e7c714f620bd2f5
Pedro <pedro.mda@gmail.com>
2021-01-12 12:19:57 +0000

    1st commit

---
+++ src/test/Main.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Violeta!");
+        int a =5;
+        int b = 10;
+        System.out.println("Soma de a com b = " + add(a, b));
+    }
+
+    private static int add(int a, int b){
+        return 0;
+    }
+}
